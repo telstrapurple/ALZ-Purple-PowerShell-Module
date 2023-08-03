5bd33fac3abc2032fed108eee5ed7699812d8181
samuel <samuel@gerges.org>
2023-08-03 15:27:12 +0930

    update

--- package_ALZ.ps1
+++ package_ALZ.ps1
@@ -6,5 +6,5 @@ param(
 New-Item "ALZ-PURPLE" -ItemType Directory -Force
 Copy-Item -Path "./src/Artifacts/*" -Destination "./ALZ-PURPLE" -Recurse -Exclude "ccReport", "testOutput"  -Force
 
-Update-ModuleManifest -Path "./ALZ-PURPLE/ALZ-PURPLE.psd1" -ModuleVersion $version -Prerelease $prerelease
+Update-ModuleManifest -Path "./ALZ/ALZ.psd1" -ModuleVersion $version -Prerelease $prerelease
 
