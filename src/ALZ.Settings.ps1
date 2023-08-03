08b875c14988b645a749f8053199b195159b6329
samuel <samuel@gerges.org>
2023-08-03 17:27:56 +0930

    update module name

--- src/ALZ.Settings.ps1
+++
@@ -1,5 +0,0 @@
-# specify the minimum required major PowerShell version that the build script should validate
-[version]$script:requiredPSVersion = '5.1.0'
-
-# specify the supported versions of ALZ-Bicep
-$script:ALZBicepSupportedReleases = @('v0.14.0', 'v0.15.0', 'v0.16.0', 'v0.16.1')
