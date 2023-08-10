31bf84aacf2f95933331eb0aaa6f0f33a852b4ad
samuel <samuel@gerges.org>
2023-08-10 15:46:00 +0930

    update

--- src/ALZPurple/Public/New-ALZEnvironment.ps1
+++ src/ALZPurple/Public/New-ALZEnvironment.ps1
@@ -70,7 +70,7 @@ function New-ALZEnvironment {
         if (-not $localSourcePackagePath) {
             Get-ALZGithubRelease -directoryForReleases $alzEnvironmentDestinationInternalCode -githubRepoUrl $bicepConfig.module_url -releases @($bicepConfig.version) | Out-String | Write-Verbose
         } else {
-            Get-ALZLocalRelease -directoryForReleases $alzEnvironmentDestinationInternalCode -localSourcePackagePath $localSourcePackagePath | Out-String | Write-Verbose
+            Get-ALZLocalRelease -directoryForReleases $alzEnvironmentDestinationInternalCode -localSourceDirectory $localSourcePackagePath -releaseVersion $alzBicepVersion | Out-String | Write-Verbose
         }
 
         Write-InformationColored "Copying ALZ-Bicep module to $alzEnvironmentDestinationInternalCode" -ForegroundColor Green -InformationAction Continue
