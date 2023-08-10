62421acc44044057b682818bc3c3f1f741715334
samuel <samuel@gerges.org>
2023-08-10 15:36:48 +0930

    update unit test

--- src/ALZPurple/Public/Get-ALZLocalRelease.ps1
+++ src/ALZPurple/Public/Get-ALZLocalRelease.ps1
@@ -3,7 +3,11 @@ function Get-ALZLocalRelease {
     param (
         [Parameter(Mandatory = $true, Position = 1, HelpMessage = "Please Provide the location of the local source directory.")]
         [string]
-        $localSourcePackagePath,
+        $localSourceDirectory,
+
+        [Parameter(Mandatory = $false, Position = 2, HelpMessage = "The current release version of the repo.")]
+        [string]
+        $releaseVersion = 'V0.1.16',
 
         [Parameter(Mandatory = $false, Position = 3, HelpMessage = "The directory to download the releases to. Defaults to the current directory.")]
         [string]
@@ -24,48 +28,47 @@ function Get-ALZLocalRelease {
         New-Item -ItemType Directory -Path $directoryForReleases | Out-String | Write-Verbose
     }
 
-    foreach ($release in $selectedReleases) {
-        # Check the firectory for this release
-        $releaseDirectory = "$directoryForReleases/$($release.tag_name)"
+    # Check the firectory for this release
+    $releaseDirectory = "$directoryForReleases/$releaseVersion"
 
-        Write-Verbose "===> Checking if directory for release version exists: $releaseDirectory"
+    Write-Verbose "===> Checking if directory for package name exists: $releaseDirectory"
 
-        if (!(Test-Path $releaseDirectory)) {
-            Write-Verbose "Directory does not exist for release $($release.tag_name), will now create: $releaseDirectory"
-            New-Item -ItemType Directory -Path $releaseDirectory | Out-String | Write-Verbose
-        }
-
-        Write-Verbose "===> Checking if any content exists inside of $releaseDirectory"
-
-        $contentInReleaseDirectory = Get-ChildItem -Path $releaseDirectory -Recurse -ErrorAction SilentlyContinue
-
-        if ($null -eq $contentInReleaseDirectory) {
-            Write-Verbose "===> Pulling and extracting release $($release.tag_name) into $releaseDirectory"
-            New-Item -ItemType Directory -Path "$releaseDirectory/tmp" | Out-String | Write-Verbose
-            #copy the zip file to the tmp directory
-            Copy-Item -Path $localSourcePackagePath -Destination "$releaseDirectory/tmp/$($release.tag_name).zip"
-
-            # Invoke-WebRequest -Uri "https://github.com/$repoOrgPlusRepo/archive/refs/tags/$($release.tag_name).zip" -OutFile "$releaseDirectory/tmp/$($release.tag_name).zip" | Out-String | Write-Verbose
-            Expand-Archive -Path "$releaseDirectory/tmp/$($release.tag_name).zip" -DestinationPath "$releaseDirectory/tmp/extracted" | Out-String | Write-Verbose
-            $extractedSubFolder = Get-ChildItem -Path "$releaseDirectory/tmp/extracted" -Directory
-
-            if ($null -ne $directoryAndFilesToKeep) {
-                foreach ($path in $directoryAndFilesToKeep) {
-                    Write-Verbose "===> Moving $path into $releaseDirectory."
-                    Move-Item -Path "$($extractedSubFolder.FullName)/$($path)" -Destination "$releaseDirectory" -ErrorAction SilentlyContinue | Out-String | Write-Verbose
-                }
-            }
-
-            if ($null -eq $directoryAndFilesToKeep) {
-                Write-Verbose "===> Moving all extracted contents into $releaseDirectory."
-                Move-Item -Path "$($extractedSubFolder.FullName)/*" -Destination "$releaseDirectory" -ErrorAction SilentlyContinue | Out-String | Write-Verbose
-            }
-
-            Remove-Item -Path "$releaseDirectory/tmp" -Force -Recurse
-
-        } else {
-            Write-Verbose "===> Content already exists in $releaseDirectory. Skipping"
-        }
+    if (!(Test-Path $releaseDirectory)) {
+        Write-Verbose "Directory does not exist for release $($releaseVersion), will now create: $releaseDirectory"
+        New-Item -ItemType Directory -Path $releaseDirectory | Out-String | Write-Verbose
     }
+
+    Write-Verbose "===> Checking if any content exists inside of $releaseDirectory"
+
+    $contentInReleaseDirectory = Get-ChildItem -Path $releaseDirectory -Recurse -ErrorAction SilentlyContinue
+
+    if ($null -eq $contentInReleaseDirectory) {
+        Write-Verbose "===> Pulling and extracting release $($releaseVersion) into $releaseDirectory"
+        New-Item -ItemType Directory -Path "$releaseDirectory/tmp" | Out-String | Write-Verbose
+        #copy the zip file to the tmp directory
+        Copy-Item -Path $releaseDirectory -Destination "$releaseDirectory/tmp/$($releaseVersion).zip" -Confirm
+
+        # Invoke-WebRequest -Uri "https://github.com/$repoOrgPlusRepo/archive/refs/tags/$($release.tag_name).zip" -OutFile "$releaseDirectory/tmp/$($release.tag_name).zip" | Out-String | Write-Verbose
+        Expand-Archive -Path "$releaseDirectory/tmp/$releaseVersion.zip" -DestinationPath "$releaseDirectory/tmp/extracted" | Out-String | Write-Verbose
+        $extractedSubFolder = Get-ChildItem -Path "$releaseDirectory/tmp/extracted" -Directory
+
+        if ($null -ne $directoryAndFilesToKeep) {
+            foreach ($path in $directoryAndFilesToKeep) {
+                Write-Verbose "===> Moving $path into $releaseDirectory."
+                Move-Item -Path "$($extractedSubFolder.FullName)/$($path)" -Destination "$releaseDirectory" -ErrorAction SilentlyContinue | Out-String | Write-Verbose
+            }
+        }
+
+        if ($null -eq $directoryAndFilesToKeep) {
+            Write-Verbose "===> Moving all extracted contents into $releaseDirectory."
+            Move-Item -Path "$($extractedSubFolder.FullName)/*" -Destination "$releaseDirectory" -ErrorAction SilentlyContinue | Out-String | Write-Verbose
+        }
+
+        Remove-Item -Path "$releaseDirectory/tmp" -Force -Recurse
+
+    } else {
+        Write-Verbose "===> Content already exists in $releaseDirectory. Skipping"
+    }
+
     return $true
 }
---
+++ src/Tests/Unit/Public/Get-ALZLocalRelease.Tests.ps1
@@ -0,0 +1,93 @@
+#-------------------------------------------------------------------------
+Set-Location -Path $PSScriptRoot
+#-------------------------------------------------------------------------
+$ModuleName = 'ALZPurple'
+$PathToManifest = [System.IO.Path]::Combine('..', '..', '..', $ModuleName, "$ModuleName.psd1")
+#-------------------------------------------------------------------------
+if (Get-Module -Name $ModuleName -ErrorAction 'SilentlyContinue') {
+    #if the module is already in memory, remove it
+    Remove-Module -Name $ModuleName -Force
+}
+Import-Module $PathToManifest -Force
+#-------------------------------------------------------------------------
+
+InModuleScope 'ALZPurple' {
+    Describe 'Get-ALZLocalRelease Function Tests' -Tag Unit {
+        BeforeAll {
+            $WarningPreference = 'SilentlyContinue'
+            $ErrorActionPreference = 'SilentlyContinue'
+        }
+        Context 'Initialize config get the correct base values' {
+            BeforeEach {
+
+                Mock -CommandName Copy-Item -ParameterFilter { $localSourcePackagePath -eq "." } -MockWith {
+                    [PSCustomObject]@{
+                        ContentLength64 = 100
+                    }
+                }
+
+                Mock -CommandName Expand-Archive -MockWith {
+                    $null
+                }
+
+                Mock -CommandName Remove-Item -MockWith {
+                    $null
+                }
+
+                Mock -CommandName Get-ChildItem -ParameterFilter { $Path -eq "output/v1.0.1" } -MockWith {
+                    $null
+                }
+
+                Mock -CommandName Get-ChildItem -ParameterFilter { $Path -eq "output/v1.0.0" } -MockWith {
+                    $null
+                }
+
+                Mock -CommandName Get-ChildItem -ParameterFilter { $Path -eq "output/v1.0.1/tmp/extracted" } -MockWith {
+                    @(
+                        [PSCustomObject]@{
+                            FullName = "Internal-Folder"
+                        }
+                    )
+                }
+
+                Mock -CommandName Get-ChildItem -ParameterFilter { $Path -eq "output/v1.0.0/tmp/extracted" } -MockWith {
+                    @(
+                        [PSCustomObject]@{
+                            FullName = "Internal-Folder"
+                        }
+                    )
+                }
+
+                Mock -CommandName New-Item -MockWith {
+                    $null
+                }
+
+                Mock -CommandName Move-Item -MockWith {
+                    $null
+                }
+
+                Mock -CommandName Write-Warning -MockWith {
+                    $null
+                }
+
+            }
+
+            It 'Should get the correct releases' {
+                Get-ALZLocalRelease -localSourceDirectory '.' -releaseVersion 'v12.0.0' | Out-String | Write-Verbose
+
+                Should -Not -Invoke Write-Warning
+            }
+
+            # It 'Should warn when you ask for a release that does not exist' {
+            #     Get-ALZGithubRelease -githubRepoUrl "http://github.com/test/repo" -releases @('v2.0.0') -directoryAndFilesToKeep @('repo-1.0.0') -directoryForReleases "output"
+            #     Should -Invoke Write-Warning
+            # }
+
+            # It 'Should download all the releases with all' {
+            #     Get-ALZGithubRelease -githubRepoUrl "http://github.com/test/repo" -releases @('all') -directoryAndFilesToKeep @('repo-1.0.0') -directoryForReleases "output"
+            #     Should -Invoke Expand-Archive -Times 2
+            #     Should -Not -Invoke Write-Warning
+            # }
+        }
+    }
+}
